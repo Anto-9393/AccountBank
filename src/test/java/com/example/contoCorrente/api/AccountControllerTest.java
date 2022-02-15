9b8f8f76cad0b71f181b813d1a9b55748df317af
Anto-9393 <giangrecouni@gmail.com>
2022-02-15 15:27:45 +0100

    ControllerTest v1.2

--- src/test/java/com/example/contoCorrente/api/AccountControllerTest.java
+++ src/test/java/com/example/contoCorrente/api/AccountControllerTest.java
@@ -27,7 +27,10 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import static org.hamcrest.Matchers.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -74,27 +77,38 @@ class AccountControllerTest {
         Conto conto = new Conto(1, 1, 500);
         when(accountService.checkMoneyById(id)).thenReturn(conto.getSaldo());
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/bank/balance/{id}", 1)).
-
-                andExpect(MockMvcResultMatchers.status().isOk());
-
-
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/bank/balance/{id}", 1)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$").value(500));
     }
 
     @Test
     public void testDepositMoney() throws Exception {
         long id = 1;
-        Movimenti movimenti = new Movimenti(1,1,Type.deposito,500);
+        Movimenti movimenti = new Movimenti(1, 1, Type.deposito, 500);
         when(movimentiRepository.save(movimenti)).thenReturn(movimenti);
 
-        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/bank/operations/{id}",1)
-                .content(asJsonString(movimenti))
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$[1].importo").exists());
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/bank/operations/{id}", 1)
+                        .content(asJsonString(movimenti))
+                        .contentType(MediaType.APPLICATION_JSON))
+                        .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+    @Test
+    public void testFiveOperatins() throws Exception {
+        List <Movimenti> operations = new ArrayList<>();
+        operations.add(new Movimenti(1,1,Type.deposito,500));
+        operations.add(new Movimenti(1,1,Type.deposito,500));
+        operations.add(new Movimenti(1,1,Type.deposito,500));
+        operations.add(new Movimenti(4,1,Type.deposito,500));
+        operations.add(new Movimenti(5,1,Type.deposito,500));
+        when(accountService.lastFiveOperations(1)).thenReturn(operations);
 
-
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/bank/operations/{id}",1))
+                .andExpect(MockMvcResultMatchers.jsonPath("$",hasSize(5)))
+                .andExpect(MockMvcResultMatchers.status().isOk());
 
     }
 }
